eb874ceee508646cea7a1772cf7936a88edac98e
ylich <vladimirylish@hotmail.com>
2025-10-22 18:48:16 +0200

    Add new use case

--- src/aplicacion/app.py
+++ src/aplicacion/app.py
@@ -5,9 +5,10 @@ from ..domain.services.tools.i_tool import Tool
 # Assuming WheaterAgent is defined in tools/wheather.py
 from ..domain.repositories.i_memory import (
     IMemory,
-)  # Assuming you have a memory manager defined
+)
+from ..domain.repositories.i_repositories import IUserRepository
 from ..domain.services.llm_manager.i_llm_manager import ILLMManager
-
+from ..domain.entities import Usuario
 GPT_MODEL = "gpt-4.1"
 
 AGENT_PROMPT = """Eres un asistente que coordina herramientas 
@@ -78,3 +79,19 @@ class Supervisor:
 
         for call in output:
             self.manage_call(call, output_text)
+
+class CreateUser:
+    def __init__(self, user_repository: IUserRepository):
+        self.user_repository = user_repository
+
+    def execute(self, id, nombre, email=None):
+        self.user_repository.create_user(
+            Usuario(id=id, nombre=nombre, email=email))
+        
+class AnswerQuestion:
+    def __init__(self, user_repository: IUserRepository, conversation_manager: IConversationManager):
+        self.conversation_manager = conversation_manager
+
+    def execute(self, question: str):
+        self.conversation_manager.answer_question(question)
+        
